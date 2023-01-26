fb5819e6d9beb201007dbcd739e8de4212e8a241
35ch <67291226+35ch@users.noreply.github.com>
2023-01-26 23:15:43 +0100

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -6,46 +6,48 @@ class TicTacToe:
         self.window = tkinter.Tk()
         self.window.resizable(0,0)
 
-        self.buttons = [[tkinter.Button(self.window) for row in range(3)] for column in range(3)]
+        self.buttons = [[tkinter.Button(self.window, disabledforeground="black") for row in range(3)] for column in range(3)]
         self.board = [[0 for row in range(3)] for column in range(3)] 
-        
-        self.players = ["X", "O"]
-        random.shuffle(self.players)
         
         for row in range(3):
             for column in range(3):
-                self.buttons[row][column].config(height = 5, width = 10, command = lambda
-                                                 row=row, column=column: self.play(row, column))
-                self.buttons[row][column].grid(row = row, column = column)
+                self.buttons[row][column].config(height=1, width=2, font=("Helvetica 40 bold"), 
+                                                 command=lambda row=row, column=column: self.play(row, column))
+                self.buttons[row][column].grid(row=row, column=column)
 
+        self.players = ["X", "O"]
+        random.shuffle(self.players)
+        
         self.window.mainloop()
 
     def play(self, row, column):
         self.buttons[row][column].config(text = self.players[0], state = "disable")
         self.board[row][column] = self.players[0]
         self.players.reverse() 
-
+        
         self.check_win()
     
-    def print_result(self):
-        print(f"{self.players[1]} won!")
-        self.window.destroy()
-    
     def check_win(self):
-        for i in range(3):
-            if self.board[i][0] == self.board[i][1] == self.board[i][2] != 0:
-                self.print_result()
-            elif self.board[0][i] == self.board[1][i] == self.board[2][i] != 0:
-                self.print_result()
- 
-        if (self.board[0][0] == self.board[1][1] == self.board[2][2] != 0 or 
-            self.board[0][2] == self.board[1][1] == self.board[2][0] != 0): 
-            self.print_result()
+        win_conditions = [
+            [self.board[0][0], self.board[0][1], self.board[0][2]],
+            [self.board[1][0], self.board[1][1], self.board[1][2]],
+            [self.board[2][0], self.board[2][1], self.board[2][2]],
+            [self.board[0][0], self.board[1][0], self.board[2][0]],
+            [self.board[0][1], self.board[1][1], self.board[2][1]],
+            [self.board[0][2], self.board[1][2], self.board[2][2]],
+            [self.board[0][0], self.board[1][1], self.board[2][2]],
+            [self.board[0][2], self.board[1][1], self.board[2][0]],
+        ]
 
-        elif (0 not in self.board[0] and 
+        for condition in win_conditions:
+            if condition[0] == condition[1] == condition[2] != 0:
+                print(f"{self.players[1]} won!")
+                exit()
+
+        if (0 not in self.board[0] and 
               0 not in self.board[1] and 
               0 not in self.board[2]): 
             print("Draw")
-            self.window.destroy()
+            exit()     
 
 TicTacToe()
