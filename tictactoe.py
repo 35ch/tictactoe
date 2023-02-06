e46c3832cd856d74f66263ea91ff59c501d7e97c
35ch <67291226+35ch@users.noreply.github.com>
2023-02-06 10:34:36 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -4,17 +4,19 @@ import random
 class TicTacToe:
     def __init__(self):
         self.window = tkinter.Tk()
-        self.window.resizable(0,0)
-
-        self.buttons = [[tkinter.Button(self.window, disabledforeground="black") for row in range(3)] for column in range(3)]
+        self.window.resizable(0,0)        
+        
         self.board = [[0 for row in range(3)] for column in range(3)] 
+        self.buttons = [[tkinter.Button(self.window, disabledforeground="black") for row in range(3)] for column in range(3)]
         
         for row in range(3):
             for column in range(3):
                 self.buttons[row][column].config(height=1, width=2, font=("Helvetica 40 bold"), 
                                                  command=lambda row=row, column=column: self.play(row, column))
                 self.buttons[row][column].grid(row=row, column=column)
-
+        
+        self.win_conditions = []
+        
         self.players = ["X", "O"]
         random.shuffle(self.players)
         
@@ -28,18 +30,14 @@ class TicTacToe:
         self.check_win()
     
     def check_win(self):
-        win_conditions = [
-            [self.board[0][0], self.board[0][1], self.board[0][2]],
-            [self.board[1][0], self.board[1][1], self.board[1][2]],
-            [self.board[2][0], self.board[2][1], self.board[2][2]],
-            [self.board[0][0], self.board[1][0], self.board[2][0]],
-            [self.board[0][1], self.board[1][1], self.board[2][1]],
-            [self.board[0][2], self.board[1][2], self.board[2][2]],
-            [self.board[0][0], self.board[1][1], self.board[2][2]],
-            [self.board[0][2], self.board[1][1], self.board[2][0]],
-        ]
-
-        for condition in win_conditions:
+        for i in range(3):
+            self.win_conditions.append([self.board[i][0], self.board[i][1], self.board[i][2]])
+            self.win_conditions.append([self.board[0][i], self.board[1][i], self.board[2][i]])
+        
+        self.win_conditions.append([self.board[0][0], self.board[1][1], self.board[2][2]])
+        self.win_conditions.append([self.board[2][0], self.board[1][1], self.board[0][2]])
+        
+        for condition in self.win_conditions:
             if condition[0] == condition[1] == condition[2] != 0:
                 print(f"{self.players[1]} won!")
                 exit()
@@ -49,5 +47,8 @@ class TicTacToe:
               0 not in self.board[2]): 
             print("Draw")
             exit()     
+        
+        else: 
+            self.win_conditions = []
 
 TicTacToe()
